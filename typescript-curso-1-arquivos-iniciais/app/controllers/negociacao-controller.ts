4ca3e30277ca75970787bc8937f3c3aeb04d1a97
Luis Fernando R. Honorato <luisferando.honorato@gmail.com>
2023-12-20 15:14:21 -0300

    Módulo 3

--- typescript-curso-1-arquivos-iniciais/app/controllers/negociacao-controller.ts
+++ typescript-curso-1-arquivos-iniciais/app/controllers/negociacao-controller.ts
@@ -1,3 +1,4 @@
+import { DiasDaSemana } from "../enums/dias-da-semana.js";
 import { Negociacao } from "../models/negociacao.js";
 import { Negociacoes } from "../models/negociacoes.js";
 import { MensagemView } from "../views/mensagem-view.js";
@@ -19,16 +20,29 @@ export class NegociacaoController {
     
     }
 
-    adiciona(): void {
+    public adiciona(): void {
+
         const negociacao = this.criaNegociacao();
+        if(!this.ehDiaUtil(negociacao.data)){
+            this.mensagemView
+                .update('Apenas negociações em dias úteis são aceitas')
+            return;
+        }
+
         this.negociacoes.adiciona(negociacao);
-        this.negociacoesView.update(this.negociacoes);
-        this.mensagemView.update('Negociação Adicionada com Sucesso!');
         this.limparFormulário();
+        this.atualizaView();
+        
+       
+    }
+
+    private ehDiaUtil(data: Date){
+        return data.getDay() > DiasDaSemana.DOMINGO 
+            && data.getDay() < DiasDaSemana.SABADO;
 
     }
 
-    criaNegociacao(): Negociacao {
+    private criaNegociacao(): Negociacao {
         const exp = /-/g;
         const date = new Date(this.inputData.value.replace(exp, ','));
         const quantidade = parseInt(this.inputQuantidade.value);
@@ -36,10 +50,15 @@ export class NegociacaoController {
         return new Negociacao(date, quantidade, valor);
     }
 
-    limparFormulário(): void {
+    private limparFormulário(): void {
         this.inputData.value = '';
         this.inputQuantidade.value = '';
         this.inputValor.value = '';
         this.inputData.focus();
+    }
+
+    private atualizaView(): void {
+        this.negociacoesView.update(this.negociacoes);
+        this.mensagemView.update('Negociação Adicionada com Sucesso!');
     }
 }
