efe1ab139e9f49808b2103666e51fe5cc9f8b424
Luis Fernando R. Honorato <luisferando.honorato@gmail.com>
2023-12-21 18:14:49 -0300

    Módulo 5

--- typescript-curso-1-arquivos-iniciais/app/controllers/negociacao-controller.ts
+++ typescript-curso-1-arquivos-iniciais/app/controllers/negociacao-controller.ts
@@ -13,14 +13,16 @@ export class NegociacaoController {
     private mensagemView = new MensagemView('#mensagemView');
 
     constructor() {
-        this.inputData = document.querySelector('#data');
-        this.inputQuantidade = document.querySelector('#quantidade');
-        this.inputValor = document.querySelector('#valor');
+        this.inputData = <HTMLInputElement>document.querySelector('#data');
+        this.inputQuantidade = document.querySelector('#quantidade') as HTMLInputElement;
+        this.inputValor = document.querySelector('#valor') as HTMLInputElement;
         this.negociacoesView.update(this.negociacoes);
     
     }
 
     public adiciona(): void {
+
+        //Comentário de teste para aprender sobre o removeComents do tsconfig.json
 
         const negociacao = Negociacao.criaDe(
             this.inputData.value,
