fbe86bf771471690c36a354ad6c5d9f63d79861c
Luis Fernando R. Honorato <luisferando.honorato@gmail.com>
2023-12-28 23:01:38 -0300

    Módulo 2

--- typescript-curso-3/app/dist/js/decorators/inspetc.js
+++ typescript-curso-3/app/dist/js/decorators/inspetc.js
@@ -1,17 +1,11 @@
-export function inspect(
-    target: any,
-    propertyKey: string,
-    descriptor: PropertyDescriptor
-) {
+export function inspect(target, propertyKey, descriptor) {
     const metodoOriginal = descriptor.value;
-    descriptor.value = function (...args: any[]) {
+    descriptor.value = function (...args) {
         console.log(`--- Método ${propertyKey}`);
         console.log(`-----Parâmetros: ${JSON.stringify(args)}`);
         const retorno = metodoOriginal.apply(this, args);
         console.log(`------- Retorno: ${JSON.stringify(retorno)}`);
-
         return retorno;
-    }
-
+    };
     return descriptor;
 }
